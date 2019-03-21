84371356593bfcc04419f8388d6d39e9f8f7024c
Dylan <dylan.jannetty@sportshubtech.com>
2019-03-21 18:28:32 -0400

    commit

---
+++ app/assets/javascripts/angular/events/EventsCtrl.js
@@ -0,0 +1,34 @@
+var app = angular.module('angularExample');
+
+app.controller('EventsCtrl', ['$scope','Event', function($scope, Event) {
+  $scope.events = Event.query();
+  $scope.addEvent = function() {
+    if (!valid()) { return false; }
+
+    Event.save($scope.event,
+      function(response, _headers) {
+        $scope.events.push(response);
+      },
+      function(response) {
+        alert('Errors: ' + response.data.errors.join('. '));
+      }
+    );
+
+    $scope.event = {};
+  };
+  
+  $scope.filterEvents = function() {
+  Event.search({query: $scope.search},
+    function(response, _headers) {
+      $scope.events = response;
+    }
+  );
+};
+
+valid = function() {
+  return !!$scope.event &&
+    !!$scope.event.name && !!$scope.event.event_date &&
+    !!$scope.event.description && !!$scope.event.place;
+}
+
+}]);
---
+++ app/assets/javascripts/angular/events/event.js
@@ -0,0 +1,12 @@
+app.factory('Event', ['$resource', function($resource) {
+  return $resource('/api/events/:id.json', { id: '@id' }, {
+    search: {
+      method: 'GET',
+      isArray: true,
+      url: '/api/events/search.json',
+      params: {
+        query: '@query'
+      }
+    }
+  });
+}]);
