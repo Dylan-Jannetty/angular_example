4f313b845ce139b714f1baf5371e483a7d04812e
Dylan <dylan.jannetty@sportshubtech.com>
2019-03-22 13:33:11 -0400

    finished tutorial

--- app/assets/javascripts/angular/events/EventsCtrl.js
+++ app/assets/javascripts/angular/events/EventsCtrl.js
@@ -1,13 +1,18 @@
-var app = angular.module('angularExample');
-
 app.controller('EventsCtrl', ['$scope','Event', function($scope, Event) {
   $scope.events = Event.query();
+  $scope.editing = {};
+  $scope.sorting = {
+    sort_by: 'name',
+    order: 'asc'
+  };
+  
   $scope.addEvent = function() {
     if (!valid()) { return false; }
 
     Event.save($scope.event,
       function(response, _headers) {
         $scope.events.push(response);
+        console.log(response);
       },
       function(response) {
         alert('Errors: ' + response.data.errors.join('. '));
@@ -17,6 +22,42 @@ app.controller('EventsCtrl', ['$scope','Event', function($scope, Event) {
     $scope.event = {};
   };
   
+  $scope.toggleForm = function(event) {
+    if(event.id === $scope.editing.id) {
+      return 'form';
+    } else {
+      return 'row';
+    }
+  };
+  
+  $scope.editEvent = function(event) {
+    $scope.editing = angular.copy(event);
+  };
+  
+  $scope.updateEvent = function(index) {
+    Event.update($scope.editing,
+    function(response, _headers) {
+      $scope.events[index] = angular.copy($scope.editing);
+      $scope.hideForm();
+    },
+    function(reponse) {
+      alert('Errors:' + response.data.errors.join('. '))
+    }
+  );
+};
+  
+  $scope.destroyEvent = function(event, index) {
+    Event.delete(event,
+      function(response, _headers) {
+        $scope.events.splice(index, 1);
+      }
+    );
+  };
+
+  $scope.hideForm = function() {
+    $scope.editing = {};
+  }
+  
   $scope.filterEvents = function() {
   Event.search({query: $scope.search},
     function(response, _headers) {
@@ -24,6 +65,33 @@ app.controller('EventsCtrl', ['$scope','Event', function($scope, Event) {
     }
   );
 };
+
+$scope.sortEvents = function(sort_by, order) {
+  if ($scope.sorting.sort_by == sort_by) {
+    order = (order == 'asc' ? 'desc' : 'asc');
+  } else if ($scope.sorting.sort_by != sort_by) {
+    order = 'asc';
+  }
+  Event.sort({ sort_by: sort_by, order: order },
+    function(response, _headers) {
+      $scope.events = response;
+      $scope.sorting = {
+        sort_by: sort_by,
+        order: order
+      }
+    }
+  );
+};
+
+$scope.updateArrowOrder = function() {
+  $scope.order = $scope.sorting.order == 'asc' ? 'up' : 'down';
+};
+
+$scope.updateArrowOrder();
+
+$scope.$watch('sorting.order', function(oldVal, newVal) {
+  $scope.updateArrowOrder();
+});
 
 valid = function() {
   return !!$scope.event &&
--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,31 +1,62 @@
 module Api
   class EventsController < ApplicationController
+    before_action :set_event, only: [:update, :destroy]
     respond_to :json
 
     def index
-      respond_with Event.all
+      respond_with Event.order("#{sort_by} #{order}")
+    end
+    
+    def sort_by
+      %w(
+        name
+        place
+        event_date
+        description
+        ).include?(params[:sort_by]) ? params[:sort_by] : 'name'
+    end
+    
+    def order
+      %w(asc desc).include?(params[:order]) ? params[:order] : 'asc'
+    end
+    
+    def destroy
+      @event.destroy
+      head :ok
+    end
+    
+    def update
+      if @event.update(event_params)
+        respond_with :api, @event, status: :ok, location: api_events_url(@event)
+      else
+        render json: { errors: @event.errors.full_messages }, status: :unprocessable_entity
+      end
     end
 
     def create
       event = Event.new(event_params)
       if event.save
-        respond_with :api, event, status: :ok, location: api_events_url
+        respond_with :api, event, status: :ok
       else
         render json: { errors: event.errors.full_messages }, status: :unprocessable_entity
       end
     end
+    
     def search
       query = params[:query]
-      events = Event.where('name LIKE ? OR place LIKE ? OR description LIKE ?',
-                       "%#{query}%", "%#{query}%", "%#{query}%")
-
-                       respond_with events
-                     end
+      events = Event.where('name LIKE ? OR place LIKE ? OR description LIKE ?', "%#{query}%", "%#{query}%", "%#{query}%")
+      respond_with events
+    end
 
     private
 
     def event_params
       params.require(:event).permit(:name, :description, :event_date, :place)
     end
+    
+    def set_event
+      @event = Event.find(params[:id])
+    end
+    
   end
 end
